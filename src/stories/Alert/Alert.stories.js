9ededbfbc202f9be0abbba73e2afe0661c137600
erolsmsr58 <erolsmsr@gmail.com>
2024-07-22 02:09:20 +0200

    Minor clean up

--- src/stories/Alert/Alert.stories.js
+++ src/stories/Alert/Alert.stories.js
@@ -24,11 +24,3 @@ export const PDSAlert = {
         bold: null
     }
 };
-
-// pds-alert	Creates an Alert, and defaults to Informational Alert styles. While this class is supported, it is recommended to use pds-alert--info instead for better semantics.
-// pds-alert--global	Creates an Global Alert, with square corners. Use in combination with any of the classes below.
-// pds-alert--info	Creates an Informational Alert
-// pds-alert--warning	Creates a Warning Alert
-// pds-alert--success	Creates a Success Alert
-// pds-alert--danger	Creates an Error Alert
-// pds-font-600	Create bold text for an Alert title
